8ecd9d36b803ae9f43d201f7a1b1367feab7b34a
DragonettEx <heartheadhead1771@gmail.com>
2025-12-09 14:56:04 +0300

    fix login

--- frontend/js/common/userDropdown.js
+++ frontend/js/common/userDropdown.js
@@ -16,7 +16,7 @@ export function renderUserDropdown(container) {
         <li><button type="button" id="userManagementBtn" class="dropdown-item d-none">Управление пользователями</button></li>
         <li><button type="button" id="advancedModeBtn" class="dropdown-item d-none">Расширенный режим</button></li>
         <li><button type="button" id="userParserBtn" class="dropdown-item d-none">Парсинг</button></li>
-        <li><a id="suka" class="dropdown-item" target="_blank" href="/instruction"> Инструкция</button></li>
+        <li><a id="instructionLink" class="dropdown-item" target="_blank" href="/instruction">Инструкция</a></li>
         <li><button type="button" id="loginBtn" class="dropdown-item">Войти</button></li>
         <li><button type="button" id="logoutBtn" class="dropdown-item text-danger d-none">Выйти</button></li>
       </ul>
